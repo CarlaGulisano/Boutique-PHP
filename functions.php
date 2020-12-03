fffcfb4b19f2a0ceb3ec75455414d6cbd52df02d
Carla <carla.gulisano@le-campus-numerique.fr>
2020-12-03 14:35:20 +0100

    Première version

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php 
+
+function displayItem1(){
+    $nom = "IPhone SE";
+    $prix = "489";
+    $img = "se.png";
+}
+
+function displayItem2(){
+    $nom = "IPad Air";
+    $prix = "669€";
+    $img = "ipad.png";
+}
+
+function displayItem3(){
+    $nom = "Apple Watch SE";
+    $prix = "299€";
+    $img = "applewatch.png";
+}
+
+?>
