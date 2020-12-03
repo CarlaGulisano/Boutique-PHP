8d7a43e3bee8201998f023cd9e70a1b3d6d42e31
Carla <carla.gulisano@le-campus-numerique.fr>
2020-12-03 16:52:51 +0100

    deuxième version

--- functions.php
+++ functions.php
@@ -1,21 +1,30 @@
 <?php 
-
+// Fonctions pour mes articles 
 function displayItem1(){
     $nom = "IPhone SE";
-    $prix = "489";
+    $prix = "489€";
     $img = "se.png";
+    echo $nom ; echo $prix ; echo $img;
 }
 
 function displayItem2(){
     $nom = "IPad Air";
     $prix = "669€";
     $img = "ipad.png";
+    echo $nom ; echo $prix ; echo $img;
 }
 
 function displayItem3(){
     $nom = "Apple Watch SE";
     $prix = "299€";
     $img = "applewatch.png";
+    echo $nom ; echo $prix ; echo $img;
 }
 
+// Fonction 
+function displayItem($nom, $prix, $img){
+    echo "<h2>" . $nom . "</h2>" ;
+    echo "<h3>" . $prix . "</h3>";
+    echo "<img src=" . $img . " style='height : 100px' >";
+}
 ?>
