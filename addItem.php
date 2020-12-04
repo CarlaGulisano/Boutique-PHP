8beb4affe4314f643d293490b0b48f0ef3ef4f7d
Carla <carla.gulisano@le-campus-numerique.fr>
2020-12-04 11:05:29 +0100

    Première version

---
+++ addItem.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+
+</head>
+
+<body>
+<!-- Formulaire + appel de la page displayItem.php -->
+<form action="displayItem.php" method="post">
+ <p>Article : <input type="text" name="nom" /></p>
+ <p><input type="submit" value="OK"></p>
+</form>
+
+
+
+</body>
+
+</html>
