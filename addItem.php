28e5dbf379dca79e2fb3cd050302cbd68d6ac043
Carla <carla.gulisano@le-campus-numerique.fr>
2020-12-07 09:38:39 +0100

    Mise à jour formulaire

--- addItem.php
+++ addItem.php
@@ -7,7 +7,8 @@
 <body>
 <!-- Formulaire + appel de la page displayItem.php -->
 <form action="displayItem.php" method="post">
- <p>Article : <input type="text" name="nom" /></p>
+ <p>Article : <input type="text" name="nom" required /></p>
+ <p>Prix : <input type="number" name="prix" value = "1" min ="1" /></p>
  <p><input type="submit" value="OK"></p>
 </form>
 
